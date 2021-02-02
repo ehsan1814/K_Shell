9752f68513b5b8d8c51d4f0df43cf95f51171f21
Mohammad Ehsan Kaji <mr.ehsan.kaji@gmail.com>
2021-02-03 00:35:13 +0330

    phase 2 added

--- K_Shell.py
+++ K_Shell.py
@@ -1,15 +1,42 @@
 import shlex
 import os
+import sys
 
 
 if __name__ == "__main__":
     while True:
+
+        #print("shell>",end="")
+        #inp = sys.stdin.readline()
+        
         inp = input("shell> ")
         ls = shlex.split(inp)
-        pid = os.fork()
-        if pid > 0:
-            os.wait()
-        else:
-            print(os.getpid(),os.getppid())
-            os.execvp(ls[0],ls)
-            
+
+        if len(ls) == 0:
+            continue
+        
+        char = ls[0]
+        print(ls)
+        #we have quit and exit function
+        if char == 'exit' :
+            quit()
+        
+        try:
+            if char == 'ls':
+                pid = os.fork()
+                if pid > 0:
+                    os.wait()
+                else:
+                    #print(os.getpid(),os.getppid())
+                    os.execvp(char,ls)
+            elif char == 'cd' or char == 'pwd' or char == 'cd..':
+                pwd = os.getcwd()
+                if char == 'pwd':
+                    print(pwd)
+                    continue
+                os.chdir(ls[1])
+                
+            else:
+                print("Command not found")
+        except Exception as e:
+            print(e)
